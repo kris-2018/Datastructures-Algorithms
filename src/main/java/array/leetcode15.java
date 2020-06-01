e1f65275f3ce930ad2adc0c4d4d6d668f434ac25
kris-2018 <kris@126.com>
2020-06-02 00:02:34 +0800

    15

--- src/main/java/array/leetcode15.java
+++ src/main/java/array/leetcode15.java
@@ -1,10 +1,10 @@
 package array;
 
-import java.util.List;
+import java.util.*;
 
 /**
  *
- * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+ * 给你一个包含 n 个整数的数组 nums，判断 nums中是否存在三个元素 a，b，c ，使得a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
  * 注意：答案中不可以包含重复的三元组。
  * 示例：
  *
@@ -17,17 +17,46 @@ import java.util.List;
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/3sum
  *
+ * 不包含重复元素，可使用Set，它的实现类有HashSet,
+ *                                  TreeSet,
+ *                                 LinkedHashSet(是HashSet的子类，但是比HashSet要多一个“记录元素添加顺序的功能”，因此它添加和删除的效率比HashSet相对低。)
  *
  */
 
 public class leetcode15 {
     public static void main(String[] args) {
+        int arr[] = {-1,0,1,2,-1,-4};
+        leetcode15 leetcode15 = new leetcode15();
 
-
+        for (List<Integer> integers : leetcode15.threeSum(arr)) {
+            System.out.println(integers);
+        }
     }
+
+
+
+    /**
+     * 这种暴力解法会超出时间限制
+     * @param nums
+     * @return
+     */
     public List<List<Integer>> threeSum(int[] nums) {
-
-
+        //int[] res;
+        Arrays.sort(nums); //一定要先排好序，不然结果是错误的
+        Set<List<Integer>> result = new LinkedHashSet<>();
+        for (int i = 0;i < nums.length - 2;i++){
+            for (int j = i + 1;j < nums.length - 1;j++){
+                for (int k = j + 1;k < nums.length;k++){
+                    if (nums[i] + nums[j] + nums[k] == 0){
+                        //int[] res = {};
+                        List<Integer> value = Arrays.asList(nums[i], nums[j], nums[k]);
+                        result.add(value);
+                    }
+                }
+            }
+        }
+        return new ArrayList<List<Integer>>(result);
     }
+
 
 }
