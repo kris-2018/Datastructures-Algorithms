ccd3c2116c332f4ce16cf4a010c633fa13fd69e6
kris-2018 <kris18@126.com>
2020-06-13 16:00:15 +0800

    旋转数组

--- src/main/java/com/leetcode/array/leetcode189.java
+++ src/main/java/com/leetcode/array/leetcode189.java
@@ -29,29 +29,71 @@ public class leetcode189 {
 
     public static void main(String[] args) {
         leetcode189 leetcode189 = new leetcode189();
-        int[] res = {1,2,3,4,5,6,7};
-        leetcode189.rotate(res, 3);
+        int[] res = {1,2,3,4};
+        leetcode189.rotate(res, 2);
+        for (int i : res) {
+            System.out.print(i + "\t");
+        }
     }
 
-    /**
-     * 递归调用
+    /** 方法一:
+     * 反转  时间复杂度 O(n), 空间复杂度 O(1)
+     * 将所有元素反转。然后反转前 k 个元素，再反转后面 n−k 个元素，就能得到想要的结果。
      * @param nums
      * @param k
      */
     public void rotate(int[] nums, int k) {
-        k %= nums.length;
-        reverse(nums, 0, nums.length - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, nums.length - 1);
+        k %= nums.length; //k = k % nums.length 取余数  这步主要是为了防止 k 超过数组的长度
+        reverse(nums, 0, nums.length - 1); //先将所有的数都反转, 收尾交换, 往中间夹
+        reverse(nums, 0, k - 1); //再将数组从0位到 k-1位交换
+        reverse(nums, k, nums.length - 1); //最后将k k位 到 末位交换, 两次交换相当于位置没变化.
     }
-
+    //数组中 两两 交换
     public void reverse(int[] nums, int start, int end) {
         while (start < end){
             int temp = nums[start];
             nums[start] = nums[end];
-            nums[end] = temp;
+            nums[end] = temp;  // 两两交换元素 位置,  往中间收缩夹紧
             start++;
             end--;
         }
     }
+
+    /**
+     * 方法二
+     * 时间复杂度 O(n), 空间复杂度 O(n)
+     * 使用额外的空间 不符合题意
+     * @param nums
+     * @param k
+     */
+    public void rotate1(int[] nums, int k) {
+        int[] new_nums = new int[nums.length];
+        //把需要旋转的元素放到新的数组中
+        for (int i = 0; i < nums.length; i++) {
+            new_nums[(i + k) % nums.length] = nums[i]; // (i+k) % nums.length  2 3 0 1
+        }
+        //将旋转好的数组依次赋值给nums数组
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = new_nums[i];
+        }
+    }
+
+    /**
+     * 暴力求解
+     * 旋转 k 次, 每次将数组旋转一个元素
+     * @param nums
+     * @param k
+     */
+    public void  rotate2(int[] nums, int k) {
+        int temp, last;
+        for (int i = 0; i < k; i++) {
+            last = nums[nums.length - 1];
+            for (int j = 0; j < nums.length; j++) {
+                temp = nums[j];
+                nums[j] = last;
+                last = temp;
+            }
+        }
+    }
+
 }
