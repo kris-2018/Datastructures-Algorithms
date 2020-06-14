dbe4ac522f33b2f32f534cbe376d45bc6627ebbd
kris-2018 <kris18@126.com>
2020-06-14 19:22:25 +0800

    定义链表中节点

--- src/main/java/com/leetcode/linkedlist/ListNode.java
+++ src/main/java/com/leetcode/linkedlist/ListNode.java
@@ -4,20 +4,26 @@ import lombok.Builder;
 import lombok.Data;
 
 /*在实体类中添加@Data @Builder注解。在需要使用到的地方该类的地方可直接使用get,set方法*/
+/**
+ * 定义节点
+ * 链表由节点构成
+ *
+ */
 @Data
 @Builder
 public class ListNode {
-    int data;       //data
-    ListNode next; //next指针
+    public int data;       //data 数据
+    public ListNode next; //next指针 指向下一个节点
 
+    public ListNode() {}
     public ListNode(int data) {
         this.data = data;
     }
-
     public ListNode(int data, ListNode next) {
         this.data = data;
         this.next = next;
     }
+
 
 /*    public int getData() {
         return data;
