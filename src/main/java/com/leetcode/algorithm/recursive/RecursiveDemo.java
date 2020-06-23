37045446822d08165988030b757d61d0e33dd7e8
kris-2018 <kris18@126.com>
2020-06-24 00:06:02 +0800

    update

---
+++ src/main/java/com/leetcode/algorithm/recursive/RecursiveDemo.java
@@ -0,0 +1,25 @@
+package com.leetcode.algorithm.recursive;
+
+/**
+ * 递归代码模板
+ * 三个思维要点:
+ * 1. 不要人肉递归(画递归状态树)
+ * 2. 找到最近最简单方法，将其拆解成可重复解决的问题（重复子问题）
+ * 3. 数学归纳法
+ *
+ */
+public class RecursiveDemo {
+/*    public void recur(int level, int param) {
+        //1. terminator 递归终结条件
+        if (level > Max_LEVEL) {
+            //process result
+            return;
+        }
+        //2. process current logic 处理当前逻辑
+        process(level, param);
+        //3. drill down  下探到下一层
+        recur(level: level + 1, newParam);
+        //4. restore current level status if needed 清理当前层
+
+    }*/
+}
