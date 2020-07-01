d396121cbf4f1061ba6ebcbec36eb031739dde61
kris-2018 <kris18@126.com>
2020-07-01 08:26:01 +0800

    分治代码模板

---
+++ src/main/java/com/leetcode/algorithm/recursive/DivideConquerDemo.java
@@ -0,0 +1,23 @@
+package com.leetcode.algorithm.recursive;
+
+public class DivideConquerDemo {
+/*
+    public void devide_conquer(String problem, String param1, String param2,...){
+        // 1. recursion terminator  分解到最后的叶子，没有子问题了
+        if (problem == null) {
+            print_result;
+            return;
+        }
+        //2. prepare data  处理逻辑
+        data = prepare_data(problem);
+        subproblems = split_problem(problem, data);
+        //3. conquer subproblems 下探到下一层，解决更细节的子问题
+        subresult1 = divide_conquer(subproblems[0], p1,...)
+        subresult2 = divide_conquer(subproblems[1], p1,...)
+        subresult3 = divide_conquer(subproblems[2], p1,...)
+        ...
+        //4. process and generate the final result
+        result = process_result(subresult1, subresult2, subresult3,...)
+        //5. revert the current level states
+    }*/
+}
