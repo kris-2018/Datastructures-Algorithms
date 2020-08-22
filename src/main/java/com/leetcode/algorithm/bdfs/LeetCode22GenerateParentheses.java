aad46e80c80154a02602ba9f60f45304d7dfe7bf
kris-2018 <kris18@126.com>
2020-08-22 09:50:32 +0800

    add

--- src/main/java/com/leetcode/algorithm/bdfs/LeetCode22GenerateParentheses.java
+++ src/main/java/com/leetcode/algorithm/bdfs/LeetCode22GenerateParentheses.java
@@ -1,4 +1,30 @@
 package com.leetcode.algorithm.bdfs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LeetCode22GenerateParentheses {
+
+    private static List<String> result;
+    public static List<String> generateParenthesis(int n) {
+        result = new ArrayList<String>();
+        generate(0, 0, n, "");
+        return result;
+    }
+
+    private static void generate(int left, int right, int n, String s) {
+        //terminator
+        if (left == n && right == n) {
+            //filter out the invalid parentheses;
+            result.add(s);
+            return;
+        }
+        //process
+        //drill down
+        if (left < n)
+            generate(left + 1, right, n, s + "(");
+        if (right < n && right < left) //剪枝
+            generate(left, right + 1, n, s + ")");
+        //restore
+    }
 }
