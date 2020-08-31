d3f7e5678cabb081ea35d7250e7c042b485b4e54
kris-2018 <kris18@126.com>
2020-08-31 08:11:46 +0800

    add

---
+++ src/main/java/com/leetcode/algorithm/bdfs/LeetCode36ValidSudoku.java
@@ -0,0 +1,129 @@
+package com.leetcode.algorithm.bdfs;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 36. 有效的数独
+ * 判断一个 9x9 的数独是否有效。只需要根据以下规则，验证已经填入的数字是否有效即可。
+ * 数字 1-9 在每一行只能出现一次。
+ * 数字 1-9 在每一列只能出现一次。
+ * 数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。
+ *
+ *
+ * 上图是一个部分填充的有效的数独。
+ * 数独部分空格内已填入了数字，空白格用 '.' 表示。
+ *
+ * 示例 1:
+ * 输入:
+ * [
+ *   ["5","3",".",".","7",".",".",".","."],
+ *   ["6",".",".","1","9","5",".",".","."],
+ *   [".","9","8",".",".",".",".","6","."],
+ *   ["8",".",".",".","6",".",".",".","3"],
+ *   ["4",".",".","8",".","3",".",".","1"],
+ *   ["7",".",".",".","2",".",".",".","6"],
+ *   [".","6",".",".",".",".","2","8","."],
+ *   [".",".",".","4","1","9",".",".","5"],
+ *   [".",".",".",".","8",".",".","7","9"]
+ * ]
+ * 输出: true
+ *
+ * 示例 2:
+ * 输入:
+ * [
+ *   ["8","3",".",".","7",".",".",".","."],
+ *   ["6",".",".","1","9","5",".",".","."],
+ *   [".","9","8",".",".",".",".","6","."],
+ *   ["8",".",".",".","6",".",".",".","3"],
+ *   ["4",".",".","8",".","3",".",".","1"],
+ *   ["7",".",".",".","2",".",".",".","6"],
+ *   [".","6",".",".",".",".","2","8","."],
+ *   [".",".",".","4","1","9",".",".","5"],
+ *   [".",".",".",".","8",".",".","7","9"]
+ * ]
+ * 输出: false
+ * 解释: 除了第一行的第一个数字从 5 改为 8 以外，空格内其他数字均与 示例1 相同。
+ *      但由于位于左上角的 3x3 宫内有两个 8 存在, 因此这个数独是无效的。
+ * 说明:
+ * 一个有效的数独（部分已被填充）不一定是可解的。
+ * 只需要根据以上规则，验证已经填入的数字是否有效即可。
+ * 给定数独序列只包含数字 1-9 和字符 '.' 。
+ * 给定数独永远是 9x9 形式的。
+ *
+ * https://leetcode.com/problems/valid-sudoku
+ */
+public class LeetCode36ValidSudoku {
+    public static void main(String[] args) {
+        char[][] board = {
+            {'5','3','.','.','7','.','.','.','.'},
+            {'6','.','.','1','9','5','.','.','.'},
+            {'.','9','8','.','.','.','.','6','.'},
+            {'8','.','.','.','6','.','.','.','3'},
+            {'4','.','.','8','.','3','.','.','1'},
+            {'7','.','.','.','2','.','.','.','6'},
+            {'.','6','.','.','.','.','2','8','.'},
+            {'.','.','.','4','1','9','.','.','5'},
+            {'.','.','.','.','8','.','.','7','9'}
+        };
+        System.out.println(isValidSudoku(board));
+    }
+
+    /**
+     * Use % for horizontal traversal. Because % increments by 1 for each j : 0%3 = 0 , 1%3 = 1, 2%3 = 2, and resets back. So this covers horizontal traversal for each block by 3 steps.
+     * Use / for vertical traversal. Because / increments by 1 after every 3 j: 0/3 = 0; 1/3 = 0; 2/3 =0; 3/3 = 1.
+     * So far, for a given block, you can traverse the whole block using just j.
+     * But because j is just 0 to 9, it will stay only first block. But to increment block, use i.
+     * To move horizontally to next block, use % again :
+     * ColIndex = 3 * i%3 (Multiply by 3 so that the next block is after 3 columns. Ie 0,0 is start of first block, second block is 0,3 (not 0,1);
+     * Similarly, to move to next block vertically, use / and multiply by 3 as explained above.
+     * @param board
+     * @return
+     */
+    public static boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            Set<Character> rows = new HashSet<>();
+            Set<Character> columns = new HashSet<>();
+            Set<Character> cube = new HashSet<>();
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.' && !rows.add(board[i][j]))
+                    return false;
+                if (board[j][i] != '.' && !columns.add(board[j][i]))
+                    return false;
+                int rowIndex = 3 * (i/3);
+                int colIndex = 3 * (i%3);
+                if (board[rowIndex + j/3][colIndex + j%3] != '.' && !cube.add(board[rowIndex + j/3][colIndex + j%3]))
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isValidSudoku2(char[][] board) {
+        Set set = new HashSet();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] !=  '.') {
+                    String b = "(" + board[i][j] + ")";
+                    if (!set.add(b + i) || !set.add(j + b) || !set.add(i/3 + b + j/3)) // 注意是 j + b
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean isValidSudoku3(char[][] board) {
+        Set set = new HashSet();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char number = board[i][j];
+                if (number != '.')
+                    if (!set.add(number + "in row" + i) || !set.add(number + "in column" + j) || !set.add(number + "in block" + i/3 + "-" + j/3))
+                        return false;
+            }
+        }
+        return true;
+    }
+
+}
---
+++ src/main/java/com/leetcode/algorithm/bdfs/Leetcode37SudokuSolver.java
@@ -0,0 +1,110 @@
+package com.leetcode.algorithm.bdfs;
+
+import java.util.Arrays;
+
+/**
+ * 37. 解数独
+ * 编写一个程序，通过已填充的空格来解决数独问题。一个数独的解法需遵循如下规则：
+ * 数字 1-9 在每一行只能出现一次。
+ * 数字 1-9 在每一列只能出现一次。
+ * 数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。
+ * 空白格用 '.' 表示。
+ *
+ * 一个数独。
+ * 答案被标成红色。
+ *
+ * Note:
+ * 给定的数独序列只包含数字 1-9 和字符 '.' 。
+ * 你可以假设给定的数独只有唯一解。
+ * 给定数独永远是 9x9 形式的。
+ *
+ *
+ */
+public class Leetcode37SudokuSolver {
+    public static void main(String[] args) {
+        char[][] board = {
+                {'5','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+        };
+        solveSudoku2(board);
+        for (char[] chars : board) {
+            for (char aChar : chars) {
+                System.out.print(aChar + ' ');
+            }
+            System.out.println();
+        }
+    }
+
+
+
+    public static void solveSudoku(char[][] board) {
+        if (board == null || board.length == 0) return;
+        solve(board);
+    }
+    private static boolean solve(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(board, i, j, c)) {
+                            board[i][j] = 'c';
+                            if (solve(board))
+                                return true;
+                            else board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    private static boolean isValid(char[][] board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] != '.' && board[i][col] == c) return false;
+            if (board[row][i] != '.' && board[row][i] == c) return false;
+            if (board[3 * (row/3) + (i/3)][3 * (col/3) + (i%3)] != '.' && board[3 * (row/3) + (i/3)][3 * (col/3) + (i%3)] == c)
+                return false;
+        }
+        return true;
+    }
+
+
+    /* 方法二 */
+    public static void solveSudoku2(char[][] board) {
+        dfs(board,0);
+    }
+    private static boolean dfs(char[][] board, int d) {
+        if (d==81) return true; //found solution
+        int i=d/9, j=d%9;
+        if (board[i][j]!='.') return dfs(board,d+1);//prefill number skip
+
+        boolean[] flag=new boolean[10];
+        validate(board,i,j,flag);
+        for (int k=1; k<=9; k++) {
+            if (flag[k]) {
+                board[i][j]=(char)('0'+k);
+                if (dfs(board,d+1)) return true;
+            }
+        }
+        board[i][j]='.'; //if can not solve, in the wrong path, change back to '.' and out
+        return false;
+    }
+    private static void validate(char[][] board, int i, int j, boolean[] flag) {
+        Arrays.fill(flag,true);
+        for (int k=0; k<9; k++) {
+            if (board[i][k]!='.') flag[board[i][k]-'0']=false;
+            if (board[k][j]!='.') flag[board[k][j]-'0']=false;
+            int r=i/3*3+k/3;
+            int c=j/3*3+k%3;
+            if (board[r][c]!='.') flag[board[r][c]-'0']=false;
+        }
+    }
+}
