6fd13fd6a7fdf8e59d5419e30a6d1580a81f3190
kris-2018 <kris18@126.com>
2020-09-03 19:46:53 +0800

    add

--- src/main/java/com/leetcode/algorithm/string/anagram/LeetCode438FindAllAnagramsInAString.java
+++ src/main/java/com/leetcode/algorithm/string/anagram/LeetCode438FindAllAnagramsInAString.java
@@ -1,5 +1,8 @@
 package com.leetcode.algorithm.string.anagram;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 438. 找到字符串中所有字母异位词
  * 给定一个字符串 s 和一个非空字符串 p，找到 s 中所有是 p 的字母异位词的子串，返回这些子串的起始索引。
@@ -38,4 +41,27 @@ package com.leetcode.algorithm.string.anagram;
  */
 
 public class LeetCode438FindAllAnagramsInAString {
+    public static void main(String[] args) {
+        String s = "cbaebabacd";
+        String p = "abc";
+        for (Integer anagram : findAnagrams(s, p)) {
+            System.out.print(anagram + "\t");
+        }
+    }
+
+    public static List<Integer> findAnagrams(String s, String p) {
+        List<Integer> list = new ArrayList<>();
+        if (s == null || s.length() == 0 || p == null || p.length() == 0) return list;
+        int[] hash = new int[256];
+        for (char c : p.toCharArray()) {
+            hash[c]++;
+        }
+        int left = 0, right = 0, count = p.length();
+        while (right < s.length()) {
+            if (hash[s.charAt(right++)]-- >= 1) count--;
+            if (count == 0) list.add(left);
+            if (right - left == p.length() && hash[s.charAt(left++)]++ >= 0) count++;
+        }
+        return list;
+    }
 }
