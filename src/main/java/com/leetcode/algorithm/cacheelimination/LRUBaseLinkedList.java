5d68858034eb75b1a8822de3eb53ad33e5de5abf
kris-2018 <kris18@126.com>
2020-09-17 19:13:16 +0800

    LRULinkedList

--- src/main/java/com/leetcode/algorithm/cacheelimination/LRUBaseLinkedList.java
+++ src/main/java/com/leetcode/algorithm/cacheelimination/LRUBaseLinkedList.java
@@ -1,5 +1,7 @@
 package com.leetcode.algorithm.cacheelimination;
 
+import java.util.Scanner;
+
 /**
  * LRU 缓存
  * 缓存是一种提高数据读取性能的技术，在硬件设计、软件开发中都有着非常广泛的应用，比如常见的 CPU 缓存、数据库缓存、浏览器缓存等等。
@@ -15,13 +17,25 @@ package com.leetcode.algorithm.cacheelimination;
  * 如果此数据之前已经被缓存在链表中了，遍历得到这个数据对应的结点，并将其从原来的位置删除，然后再插入到链表的头部。
  * 如果此数据没有在缓存链表中,，则将此结点插入到链表的头部；
  * 如果此时缓存超过容量，则链表尾结点删除。
+ *  https://github.com/kris-2018/algo/blob/master/java/06_linkedlist/LRUBaseLinkedList.java
+ *
  */
 public class LRUBaseLinkedList<T> {
 
-    private final static Integer DEFAULT_CAPACITY = 10;
-    private SNode<T> headNode;
-    private Integer length;
-    private Integer capacity;
+    public static void main(String[] args) {
+        LRUBaseLinkedList list = new LRUBaseLinkedList();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            list.add(sc.nextInt());
+            list.printAll();
+        }
+    }
+
+
+    private final static Integer DEFAULT_CAPACITY = 10; //链表容量
+    private SNode<T> headNode; //头结点
+    private Integer length; //长度
+    private Integer capacity; //容量
 
     public LRUBaseLinkedList() {
         this.headNode = new SNode<>();
@@ -33,6 +47,71 @@ public class LRUBaseLinkedList<T> {
         this.headNode = new SNode<>();
         this.length = 0;
         this.capacity = capacity;
+    }
+    public void add(T data) {
+        SNode preNode = findPreNode(data);
+        // 链表中存在，删除原数据，再插入到链表的头部
+        if (preNode != null) {
+            deleteElemOptim(preNode);
+            intsertElemAtBegin(data);
+        } else {
+            if (length > capacity) {
+                //删除尾结点
+                deleteElemAtEnd();
+            }
+            intsertElemAtBegin(data);
+        }
+    }
+    /* 删除尾结点 */
+    private void deleteElemAtEnd() {
+        SNode ptr = headNode;
+        // 空链表直接返回
+        if (ptr.getNext() == null) {
+            return;
+        }
+        // 倒数第二个结点
+        while (ptr.getNext().getNext() != null) {
+            ptr = ptr.getNext();
+        }
+
+        SNode tmp = ptr.getNext();
+        ptr.setNext(null);
+        tmp = null;
+        length--;
+    }
+    /* 链表头部插入节点 */
+    private void intsertElemAtBegin(T data) {
+        SNode next = headNode.getNext();
+        headNode.setNext(new SNode(data, next));
+        length++;
+    }
+    /* 删除preNode结点下一个元素 */
+    private void deleteElemOptim(SNode preNode) {
+        SNode temp = preNode.getNext();
+        preNode.setNext(temp.getNext());
+        temp = null;
+        length--;
+    }
+
+    /* 获取查找到元素的前一个结点 */
+    private SNode findPreNode(T data) {
+        SNode node = headNode;
+        while (node.getNext() != null) {
+            if (data.equals(node.getNext().getElement())) {
+                return node;
+            }
+            node = node.getNext();
+        }
+        return null;
+    }
+
+    private void printAll() {
+        SNode node = headNode.getNext();
+        while (node != null) {
+            System.out.print(node.getElement() + ",");
+            node = node.getNext();
+        }
+        System.out.println();
     }
 
 }
